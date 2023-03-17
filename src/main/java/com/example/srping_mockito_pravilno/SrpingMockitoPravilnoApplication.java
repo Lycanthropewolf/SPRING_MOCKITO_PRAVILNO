62a14ed6133d3d5c1bfc53bbb5fc0dd7324c7ec4
Антон <antonolegovich2017@icloud.com>
2023-03-17 15:23:03 +0600

    dz

---
+++ src/main/java/com/example/srping_mockito_pravilno/SrpingMockitoPravilnoApplication.java
@@ -0,0 +1,13 @@
+package com.example.srping_mockito_pravilno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrpingMockitoPravilnoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SrpingMockitoPravilnoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/srping_mockito_pravilno/SrpingMockitoPravilnoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.srping_mockito_pravilno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SrpingMockitoPravilnoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
